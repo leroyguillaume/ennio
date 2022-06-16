2f8edfcb4b6a2fdfe317f25fbbfd25d117c34fba
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-06-16 19:52:31 +0200

    feat: implement workflow mod
    build: add log dependency
    build: add simplelog dev dependency
    test: add LogAsserter
    test: add action_stub macro

--- ennio_lib/src/lib.rs
+++ ennio_lib/src/lib.rs
@@ -16,3 +16,55 @@ macro_rules! vars {
         Vars::from([$((String::from($name), $value)),*])
     };
 }
+
+#[cfg(test)]
+mod test {
+    use std::io::{self, Write};
+
+    macro_rules! action_stub {
+        ($name:expr, $run_fn:expr) => {
+            Box::new(ActionStub::new($name, Box::new($run_fn)))
+        };
+    }
+
+    pub(crate) use action_stub;
+
+    pub struct LogAsserter {
+        expected: Vec<String>,
+        line: usize,
+        buf: Vec<u8>,
+    }
+
+    impl LogAsserter {
+        pub fn new(logs: Vec<String>) -> Self {
+            Self {
+                expected: logs,
+                line: 0,
+                buf: vec![],
+            }
+        }
+    }
+
+    impl Write for LogAsserter {
+        fn flush(&mut self) -> io::Result<()> {
+            Ok(())
+        }
+
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.buf.extend_from_slice(buf);
+            let log = String::from_utf8_lossy(&self.buf);
+            if log.ends_with('\n') {
+                if self.line >= self.expected.len() {
+                    panic!("unexpected log line: {}", log)
+                }
+                // we skip 9 first characters because it is the hour
+                // and the last one because it is \n
+                let log = &log[9..log.len() - 1];
+                assert_eq!(log, self.expected[self.line]);
+                self.line += 1;
+                self.buf = vec![];
+            }
+            Ok(buf.len())
+        }
+    }
+}
--- ennio_lib/src/workflow.rs
+++ ennio_lib/src/workflow.rs
@@ -1,15 +1,196 @@
+use crate::{action::*, context::*};
+use log::info;
+
 pub struct Workflow {
     name: String,
+    actions: Vec<Box<dyn Action>>,
 }
 
 impl Workflow {
     pub fn new(name: &str) -> Self {
         Self {
             name: String::from(name),
+            actions: vec![],
         }
     }
 
     pub fn name(&self) -> &str {
         &self.name
+    }
+
+    pub fn run(&self) -> Outputs {
+        let mut ctx = Context::new(self);
+        for action in self.actions.iter() {
+            let action_name = action.name();
+            info!("[{}] Executing action '{}'", self.name, action_name);
+            let output = action.run(&ctx);
+            info!(
+                "[{}] Action '{}' terminated with status: {}",
+                self.name,
+                action_name,
+                output.status()
+            );
+            ctx.update(action_name, output);
+        }
+        ctx.take_outputs()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use crate::*;
+
+    mod workflow {
+        use super::*;
+
+        mod new {
+            use super::*;
+
+            #[test]
+            fn should_return_workflow() {
+                let name = "workflow1";
+                let workflow = Workflow::new(name);
+                assert_eq!(workflow.name, name);
+                assert!(workflow.actions.is_empty());
+            }
+        }
+
+        mod name {
+            use super::*;
+
+            #[test]
+            fn should_return_name() {
+                let expected = "workflow1";
+                let workflow = Workflow {
+                    name: String::from(expected),
+                    actions: vec![],
+                };
+                assert_eq!(workflow.name(), expected);
+            }
+        }
+
+        mod run {
+            use super::*;
+            use crate::{action::test::*, test::*, var::*};
+            use simplelog::{Config, LevelFilter, WriteLogger};
+
+            #[test]
+            fn should_return_outputs() {
+                let workflow_name = "workflow1";
+                let action1_name = "action1";
+                let action1_status = Status::Unchanged;
+                let action1_output = Output::new(action1_status).add_var("foo1", Var::Integer(15));
+                let action2_name = "action2";
+                let action2_status = Status::Changed;
+                let action2_output =
+                    Output::new(action2_status).add_var("foo2", Var::Boolean(true));
+                let action3_name = "action3";
+                let action3_status = Status::Failed;
+                let action3_output =
+                    Output::new(action3_status).add_var("foo3", Var::String(String::from("bar1")));
+                let action4_name = "action4";
+                let action4_status = Status::Skipped;
+                let action4_output = Output::new(action4_status).add_var(
+                    "foo4",
+                    Var::Hash(vars!(String::from("foo4"), Var::Integer(15))),
+                );
+                let expected = outputs!(
+                    action1_name,
+                    action1_output.clone(),
+                    action2_name,
+                    action2_output.clone(),
+                    action3_name,
+                    action3_output.clone(),
+                    action4_name,
+                    action4_output.clone()
+                );
+                let action1 = action_stub!(action1_name, {
+                    let action1_output = action1_output.clone();
+                    move |ctx| {
+                        assert!(ctx.outputs().is_empty());
+                        action1_output.clone()
+                    }
+                });
+                let action2 = action_stub!(action2_name, {
+                    let action1_output = action1_output.clone();
+                    let action2_output = action2_output.clone();
+                    move |ctx| {
+                        let expected = outputs!(action1_name, action1_output.clone());
+                        assert_eq!(ctx.outputs().clone(), expected);
+                        action2_output.clone()
+                    }
+                });
+                let action3 = action_stub!(action3_name, {
+                    let action1_output = action1_output.clone();
+                    let action2_output = action2_output.clone();
+                    let action3_output = action3_output.clone();
+                    move |ctx| {
+                        let expected = outputs!(
+                            action1_name,
+                            action1_output.clone(),
+                            action2_name,
+                            action2_output.clone()
+                        );
+                        assert_eq!(ctx.outputs().clone(), expected);
+                        action3_output.clone()
+                    }
+                });
+                let action4 = action_stub!(action4_name, move |ctx| {
+                    let expected = outputs!(
+                        action1_name,
+                        action1_output.clone(),
+                        action2_name,
+                        action2_output.clone(),
+                        action3_name,
+                        action3_output.clone()
+                    );
+                    assert_eq!(ctx.outputs().clone(), expected);
+                    action4_output.clone()
+                });
+                let workflow = Workflow {
+                    name: String::from(workflow_name),
+                    actions: vec![action1, action2, action3, action4],
+                };
+                let logs = vec![
+                    format!(
+                        "[INFO] [{}] Executing action '{}'",
+                        workflow_name, action1_name
+                    ),
+                    format!(
+                        "[INFO] [{}] Action '{}' terminated with status: {}",
+                        workflow_name, action1_name, action1_status
+                    ),
+                    format!(
+                        "[INFO] [{}] Executing action '{}'",
+                        workflow_name, action2_name
+                    ),
+                    format!(
+                        "[INFO] [{}] Action '{}' terminated with status: {}",
+                        workflow_name, action2_name, action2_status
+                    ),
+                    format!(
+                        "[INFO] [{}] Executing action '{}'",
+                        workflow_name, action3_name
+                    ),
+                    format!(
+                        "[INFO] [{}] Action '{}' terminated with status: {}",
+                        workflow_name, action3_name, action3_status
+                    ),
+                    format!(
+                        "[INFO] [{}] Executing action '{}'",
+                        workflow_name, action4_name
+                    ),
+                    format!(
+                        "[INFO] [{}] Action '{}' terminated with status: {}",
+                        workflow_name, action4_name, action4_status
+                    ),
+                ];
+                let log_asserter = LogAsserter::new(logs);
+                WriteLogger::init(LevelFilter::Trace, Config::default(), log_asserter).unwrap();
+                let outputs = workflow.run();
+                assert_eq!(outputs, expected);
+            }
+        }
     }
 }
