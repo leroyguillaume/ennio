abc05b55908593605898f23d4648aaafa05f988f
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-06-20 21:49:19 +0200

    refactor(var): replace Value::List by Array struct

--- ennio_lib/src/var.rs
+++ ennio_lib/src/var.rs
@@ -1,5 +1,22 @@
 use std::collections::HashMap;
 
+#[macro_export]
+macro_rules! array {
+    ($($items:expr),*) => {
+        Array::from(vec![$($items),*])
+    };
+}
+
+macro_rules! impl_primitive_from_for_array {
+    ($type:ty) => {
+        impl From<Vec<$type>> for Array {
+            fn from(items: Vec<$type>) -> Self {
+                Self(items.into_iter().map(|item| Value::from(item)).collect())
+            }
+        }
+    };
+}
+
 macro_rules! value_from {
     ($type:ty, $value:expr) => {
         impl From<$type> for Value {
@@ -18,13 +35,40 @@ macro_rules! value_from {
     };
 }
 
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct Array(Vec<Value>);
+
+impl Array {
+    pub fn new() -> Self {
+        Self(vec![])
+    }
+}
+
+impl_primitive_from_for_array!(bool);
+impl_primitive_from_for_array!(u8);
+impl_primitive_from_for_array!(u16);
+impl_primitive_from_for_array!(u32);
+impl_primitive_from_for_array!(u64);
+impl_primitive_from_for_array!(i8);
+impl_primitive_from_for_array!(i16);
+impl_primitive_from_for_array!(i32);
+impl_primitive_from_for_array!(i64);
+impl_primitive_from_for_array!(String);
+impl_primitive_from_for_array!(&str);
+
+impl From<Vec<Value>> for Array {
+    fn from(items: Vec<Value>) -> Self {
+        Self(items)
+    }
+}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Value {
     Bool(bool),
     PositiveInt(u64),
     NegativeInt(i64),
     String(String),
-    List(Vec<Value>),
+    Array(Array),
     Hash(Hash),
 }
 
@@ -50,6 +94,47 @@ pub type Hash = HashMap<String, Value>;
 #[cfg(test)]
 mod test {
     use super::*;
+
+    mod array {
+        use super::*;
+
+        mod from {
+            use super::*;
+
+            macro_rules! test_primitive {
+                ($name:ident, $expected:expr) => {
+                    #[test]
+                    fn $name() {
+                        let expected: Vec<Value> = $expected
+                            .into_iter()
+                            .map(|item| Value::from(item))
+                            .collect();
+                        let array = Array::from($expected.to_vec());
+                        assert_eq!(array.0, expected);
+                    }
+                };
+            }
+
+            test_primitive!(bool, [true]);
+            test_primitive!(u8, [1]);
+            test_primitive!(u16, [1]);
+            test_primitive!(u32, [1]);
+            test_primitive!(u64, [1]);
+            test_primitive!(i8, [-1]);
+            test_primitive!(i16, [-1]);
+            test_primitive!(i32, [-1]);
+            test_primitive!(i64, [-1]);
+            test_primitive!(string, [String::from("val")]);
+            test_primitive!(str, ["val"]);
+
+            #[test]
+            fn value() {
+                let expected = vec![Value::Bool(true)];
+                let array = Array::from(expected.clone());
+                assert_eq!(array.0, expected);
+            }
+        }
+    }
 
     mod value {
         use super::*;
