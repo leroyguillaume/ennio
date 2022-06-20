77390782398cea2b48f03d12451b9a3281dfe871
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-06-20 22:04:25 +0200

    refactor(var): refactor from_impl macro
    refactor(var): rename from_impl to impl_primitive_from_for_value
    test(var): refactor Value::from tests

--- ennio_lib/src/var.rs
+++ ennio_lib/src/var.rs
@@ -17,19 +17,11 @@ macro_rules! impl_primitive_from_for_array {
     };
 }
 
-macro_rules! value_from {
-    ($type:ty, $value:expr) => {
+macro_rules! impl_primitive_from_for_value {
+    ($type:ty, $value:expr $(, $as:ty)?) => {
         impl From<$type> for Value {
             fn from(val: $type) -> Self {
-                $value(val)
-            }
-        }
-    };
-
-    ($type:ty, $value:expr, $target_type:ty) => {
-        impl From<$type> for Value {
-            fn from(val: $type) -> Self {
-                $value(val as $target_type)
+                $value(val $(as $as)?)
             }
         }
     };
@@ -72,16 +64,16 @@ pub enum Value {
     Hash(Hash),
 }
 
-value_from!(bool, Value::Bool, bool);
-value_from!(u8, Value::PositiveInt, u64);
-value_from!(u16, Value::PositiveInt, u64);
-value_from!(u32, Value::PositiveInt, u64);
-value_from!(u64, Value::PositiveInt);
-value_from!(i8, Value::NegativeInt, i64);
-value_from!(i16, Value::NegativeInt, i64);
-value_from!(i32, Value::NegativeInt, i64);
-value_from!(i64, Value::NegativeInt);
-value_from!(String, Value::String);
+impl_primitive_from_for_value!(bool, Value::Bool, bool);
+impl_primitive_from_for_value!(u8, Value::PositiveInt, u64);
+impl_primitive_from_for_value!(u16, Value::PositiveInt, u64);
+impl_primitive_from_for_value!(u32, Value::PositiveInt, u64);
+impl_primitive_from_for_value!(u64, Value::PositiveInt);
+impl_primitive_from_for_value!(i8, Value::NegativeInt, i64);
+impl_primitive_from_for_value!(i16, Value::NegativeInt, i64);
+impl_primitive_from_for_value!(i32, Value::NegativeInt, i64);
+impl_primitive_from_for_value!(i64, Value::NegativeInt);
+impl_primitive_from_for_value!(String, Value::String);
 
 impl From<&str> for Value {
     fn from(val: &str) -> Self {
@@ -142,31 +134,33 @@ mod test {
         mod from {
             use super::*;
 
-            macro_rules! test {
-                ($name:ident, $entry:expr, $expected:expr) => {
+            macro_rules! test_primitive {
+                ($name:ident, $value:expr, $expected:expr $(, $as:ty)?) => {
                     #[test]
                     fn $name() {
-                        let val = Value::from($entry);
-                        assert_eq!(val, $expected);
+                        let val = Value::from($expected);
+                        assert_eq!(val, $value($expected $(as $as)?));
                     }
                 };
             }
 
-            test!(bool, true, Value::Bool(true));
-            test!(u8, 1u8, Value::PositiveInt(1));
-            test!(u16, 1u16, Value::PositiveInt(1));
-            test!(u32, 1u32, Value::PositiveInt(1));
-            test!(u64, 1u64, Value::PositiveInt(1));
-            test!(i8, -1i8, Value::NegativeInt(-1));
-            test!(i16, -1i16, Value::NegativeInt(-1));
-            test!(i32, -1i32, Value::NegativeInt(-1));
-            test!(i64, -1i64, Value::NegativeInt(-1));
-            test!(
-                string,
-                String::from("val"),
-                Value::String(String::from("val"))
-            );
-            test!(str, "val", Value::String(String::from("val")));
+            test_primitive!(bool, Value::Bool, true);
+            test_primitive!(u8, Value::PositiveInt, 1u8, u64);
+            test_primitive!(u16, Value::PositiveInt, 1u16, u64);
+            test_primitive!(u32, Value::PositiveInt, 1u32, u64);
+            test_primitive!(u64, Value::PositiveInt, 1u64);
+            test_primitive!(i8, Value::NegativeInt, -1i8, i64);
+            test_primitive!(i16, Value::NegativeInt, -1i16, i64);
+            test_primitive!(i32, Value::NegativeInt, -1i32, i64);
+            test_primitive!(i64, Value::NegativeInt, -1i64);
+            test_primitive!(string, Value::String, String::from("val"));
+
+            #[test]
+            fn str() {
+                let expected = "val";
+                let val = Value::from(expected);
+                assert_eq!(val, Value::String(expected.into()));
+            }
         }
     }
 }
